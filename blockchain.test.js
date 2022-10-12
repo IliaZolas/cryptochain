a410ae1e925e40e4d9d9ace5db1b8c94b42febea
Ilia Zolas <ilia@thedmcsa.co.za>
2022-10-12 16:36:58 +0200

    added blockchain tests

---
+++ blockchain.test.js
@@ -0,0 +1,101 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain', () => {
+    let blockchain, newChain, originalChain;
+
+    beforeEach(()=> {
+        blockchain = new Blockchain();
+        newChain = new Blockchain();
+        originalChain = blockchain.chain;
+    });
+
+    it('contains a `chain`array instance', () => {
+        expect(blockchain.chain instanceof Array).toBe(true);
+    });
+
+    it('starts with genesis block', ()=> {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('adds a new block to the chain', () => {
+        const newData = 'foo bar';
+        blockchain.addBlock({ data: newData });
+        expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+    });
+
+    describe('isValidChain()', () => {
+        describe('when the chain does not start with the genesis block', () => {
+            it('returns false', () => {
+                blockchain.chain[0] = {data: 'fake-genesis'};
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+        
+    describe('when the chain starts with the genesis block and has mulitple blocks', () => {
+        beforeEach(()=>{
+            blockchain.addBlock({data: 'Bears'});
+            blockchain.addBlock({data: 'Bears'});
+            blockchain.addBlock({data: 'Battlestar'});
+        });
+        describe('and a lastHash reference ahas changed', () => {
+            it('returns false', () => {
+                blockchain.chain[2].lastHash = 'broken-lastHash';
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
+        });
+
+            describe('and the chain contains a block with an invalid field', () => {
+                it('returns false', () => {
+                    blockchain.chain[2].data = 'some-bad-data';
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
+            });
+
+            describe('and the chain does not contain any invalid blocks', () => {
+                it('returns true', () => {
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+                });
+            });
+        });
+    });
+
+    describe('replaceChain()', ()=>{
+        describe('when the new chain is not longer', () => {
+            it('does not replace the chain', () => {
+                newChain.chain[0] = { new: 'chain' };
+
+                blockchain.replaceChain(newChain.chain);
+
+                expect(blockchain.chain).toEqual(originalChain);
+            });
+        });
+
+        describe('when the new chain is longer', () => {
+            beforeEach(() => {
+                blockchain.addBlock({data: 'Bears'});
+                blockchain.addBlock({data: 'BeaTs'});
+                blockchain.addBlock({data: 'Battlestar'});
+            });
+
+            describe('and the chain is invalid', () => {
+                it('does not replace the chain', () => {
+                    newChain.chain[2] = 'some-hash-here-evil';
+                    // newChain.chain[2].hash = 'some-hash-here-evil';
+
+                    blockchain.replaceChain(newChain.chain);
+
+                    expect(blockchain.chain).toEqual(originalChain);
+                });
+            });
+
+            describe('and the chain is valid', () => {
+                it('replaces the cahin', () => {
+                    blockchain.replaceChain(newChain.chain);
+
+                    expect(blockchain.chain).toEqual(originalChain);
+                });
+            });
+        });
+    });
+});
